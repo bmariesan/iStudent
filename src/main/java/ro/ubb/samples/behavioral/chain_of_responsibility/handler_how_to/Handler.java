bc0acab44a5532175804b62169fb6fa3fd8a4e63
Mihnea <lzr_mihnea@yahoo.com>
2017-10-15 18:22:51 +0300

    [NoRef0][course] added chain of responsibility code

---
+++ src/main/java/ro/ubb/samples/behavioral/chain_of_responsibility/handler_how_to/Handler.java
@@ -0,0 +1,46 @@
+class Handler {
+    private final static Random RANDOM = new Random();
+    private static int nextID = 1;
+    private int id = nextID++;
+    private Handler nextInChain;
+
+    public void add(Handler next) {
+        if (nextInChain == null) {
+            nextInChain = next;
+        } else {
+            nextInChain.add(next);
+        }
+    }
+
+    public void wrapAround(Handler root) {
+        if (nextInChain == null) {
+            nextInChain = root;
+        } else {
+            nextInChain.wrapAround(root);
+        }
+    }
+
+    public void execute(int num) {
+        if (RANDOM.nextInt(4) != 0) {
+            System.out.println("   " + id + "-busy  ");
+            nextInChain.execute(num);
+        } else {
+            System.out.println(id + "-handled-" + num);
+        }
+    }
+}
+
+public class ChainDemo {
+    public static void main(String[] args) {
+        Handler rootChain = new Handler();
+        rootChain.add(new Handler());
+        rootChain.add(new Handler());
+        rootChain.add(new Handler());
+        rootChain.wrapAround(rootChain);
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Operation #" + i + ":");
+            rootChain.execute(i);
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/ro/ubb/samples/behavioral/chain_of_responsibility/purchase_power/PurchasePower.java
@@ -0,0 +1,20 @@
+abstract class PurchasePower {
+
+ protected static final double BASE = 500;
+ protected PurchasePower successor;
+
+ abstract protected double getAllowable();
+ abstract protected String getRole();
+
+ public void setSuccessor(PurchasePower successor) {
+  this.successor = successor;
+ }
+
+ public void processRequest(PurchaseRequest request) {
+  if (request.getAmount() < this.getAllowable()) {
+   System.out.println(this.getRole() + " will approve $" + request.getAmount());
+  } else if (successor != null) {
+   successor.processRequest(request);
+  }
+ }
+}
