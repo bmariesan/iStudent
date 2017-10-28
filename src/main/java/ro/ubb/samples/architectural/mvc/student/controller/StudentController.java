9ca2ec2c8869077ede63c8d9f99c65a8b1fcebbf
Mihnea <lzr_mihnea@yahoo.com>
2017-10-28 17:09:24 +0300

    [course 5, 6] added mvc samples

---
+++ src/main/java/ro/ubb/samples/architectural/mvc/student/controller/StudentController.java
@@ -0,0 +1,34 @@
+package ro.ubb.samples.architectural.mvc.student.controller;
+
+import ro.ubb.samples.architectural.mvc.student.model.Student;
+import ro.ubb.samples.architectural.mvc.student.view.StudentView;
+
+public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setStudentName(String name){
+        model.setName(name);
+    }
+
+    public String getStudentName(){
+        return model.getName();
+    }
+
+    public void setStudentRollNo(String rollNo){
+        model.setRollNo(rollNo);
+    }
+
+    public String getStudentRollNo(){
+        return model.getRollNo();
+    }
+
+    public void updateView(){
+        view.printStudentDetails(model.getName(), model.getRollNo());
+    }
+}
