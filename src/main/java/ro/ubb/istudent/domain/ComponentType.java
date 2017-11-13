b8d9a775782546c677a9d745441e567b0f39c7e3
Alexandru Stoica <stoica.alexandru2000@gmail.com>
2017-11-13 21:13:55 +0200

    Grading Criteria Builder

---
+++ src/main/java/ro/ubb/istudent/domain/ComponentType.java
@@ -0,0 +1,12 @@
+package ro.ubb.istudent.domain;
+
+/**
+ * <p>Describes the component's type. (e.g. FINAL_EXAM)</p>
+ *
+ * @author Alexandru Stoica
+ * @version 1.0
+ */
+
+public enum ComponentType {
+    FINAL_EXAM, PARTIAL_EXAM, ASSIGNMENT
+}
