e4e72302e708270571f8b215b7b1e6e5c802b5a2
DanielGuramulta <daniel.guramulta@analog.com>
2017-11-22 18:59:18 +0200

    IReportService: created report service
    Signed-off-by: DanielGuramulta <daniel.guramulta@analog.com>

---
+++ src/main/java/ro/ubb/istudent/service/IReportService.java
@@ -0,0 +1,12 @@
+package ro.ubb.istudent.service;
+
+import com.mongodb.util.JSON;
+
+/**
+ * Created by Guramulta Daniel on 11/22/2017.
+ */
+public interface IReportService {
+    JSON generateJSONReport(Report raport);
+    void generatePDFReport(Report raport, String file);
+    String generateHTMLReport(Report report);
+}
