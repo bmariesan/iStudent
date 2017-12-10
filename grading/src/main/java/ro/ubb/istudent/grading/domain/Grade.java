99419e44f0371076c3f9bdbdb079bcc5821fe924
wilversings <aiordachioaei.marius@gmail.com>
2017-12-10 17:34:20 +0200

    Added entities

---
+++ grading/src/main/java/ro/ubb/istudent/grading/domain/Grade.java
@@ -0,0 +1,15 @@
+package ro.ubb.istudent.grading.domain;
+
+import java.util.Date;
+
+/**
+ * Created by Marius on 10.12.2017.
+ */
+public class Grade {
+
+    public Double value;
+    public Date date;
+    public Teacher teacher;
+    public Student student;
+
+}
