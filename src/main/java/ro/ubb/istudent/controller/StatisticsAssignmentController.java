d4c55c464a8fc3be307ef26504604387bb6e69b9
Truta Diana Denisa <truta_diana@yahoo.com>
2017-12-13 22:08:49 +0200

    rest controller added

--- src/main/java/ro/ubb/istudent/controller/StatisticsAssignmentController.java
+++ src/main/java/ro/ubb/istudent/controller/StatisticsAssignmentController.java
@@ -1,20 +1,20 @@
 package ro.ubb.istudent.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import ro.ubb.istudent.service.AssignmentService;
 
-@Controller
+@RestController
 public class StatisticsAssignmentController {
 
     @Autowired
     private AssignmentService assignmentService;
 
     @RequestMapping(value = "/teachers", method = RequestMethod.GET)
-    public String getStatisticsFeedbackFromTeachers() {
-        return String.valueOf(assignmentService.getNumberOfAssignmentsHavingFeedbackFromTeachers());
+    public long getStatisticsFeedbackFromTeachers() {
+            return assignmentService.getNumberOfAssignmentsHavingFeedbackFromTeachers();
     }
 
 }
