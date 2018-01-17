eebb877f6daf8e6622ad7616ab640b93386b3df5
Teodora <teodora_berende@yahoo.com>
2018-01-17 16:32:24 +0200

    no message

--- src/main/java/ro/ubb/istudent/repository/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.ubb.istudent.repository;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import ro.ubb.istudent.domain.IStatistic;
-
-public interface CourseRepository extends MongoRepository<IStatistic, ObjectId> {
-}
