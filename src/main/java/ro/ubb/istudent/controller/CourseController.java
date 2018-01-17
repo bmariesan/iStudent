fb3dc95c4582bd163e391a7e18a4f0cdd2718f2e
mirko <mirceadino97@gmail.com>
2018-01-17 14:04:27 +0200

    Remove Greeting

--- src/main/java/ro/ubb/istudent/controller/CourseController.java
+++ src/main/java/ro/ubb/istudent/controller/CourseController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CourseController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
 
     @RequestMapping(value = "/allCourses", method = RequestMethod.GET)
     public String allCourses() { return "allCourses"; }
--- src/main/java/ro/ubb/istudent/repository/StudentRepository.java
+++ src/main/java/ro/ubb/istudent/repository/StudentRepository.java
@@ -2,9 +2,7 @@ package ro.ubb.istudent.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import ro.ubb.istudent.domain.GreetingEntity;
 import ro.ubb.istudent.domain.StudentEntity;
-import ro.ubb.samples.architectural.mvc.student.model.Student;
 
 import java.util.Optional;
 
