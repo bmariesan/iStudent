fd56cda71f6f76933b058be05944d28fa3bae164
DESKTOP-37TE795\BIANCA <nbie1961@scs.ubbcluj.ro>
2018-01-26 21:51:36 +0200

    Edit/delete almost complete

--- src/main/webapp/js/news.js
+++ src/main/webapp/js/news.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var url = "http://localhost:8080/api/news/getall";
+    $('#update-btn').hide();
    // alert(url);
     function getall() {
         $("tbody").empty();
@@ -13,7 +14,9 @@ $(document).ready(function(){
                     var msg= '<td>'+value.message + '</td>';
                     var tch= '<td>'+value.teacher + '</td>';
                     var crs= '<td>'+value.course + '</td>';
-                    $("#list").append('<tr>'+'<td>' + value.title + '</td>'+ msg+tch+crs+'<td><button type="submit" class="btn btn-default">Delete</button></td><td><button type="submit" class="btn btn-default">Update</button></td></tr>');
+                    var delClk = 'onclick=\"deleteNew(\''+value.id+'\');\"';
+                    var updClk= "onclick=\"updateNew('"+value.id+"','"+value.title+"','"+value.message+"','"+value.course+"')\" ";
+                    $("#list").append('<tr>'+'<td>' + value.title + '</td>'+ msg+tch+crs+'<td><button type="submit" class="btn btn-default" '+delClk+'>Delete</button></td><td><button type="submit" class="btn btn-default" '+updClk+'>Update</button></td></tr>');
                     //alert(value);
                 });
             },
@@ -27,8 +30,8 @@ $(document).ready(function(){
     function postNews() {
         //alert('akolo');
         var m=$('#message').val();
-        var course = $('#title').val();
-        var title = $('#course').val();
+        var title = $('#title').val();
+        var course = $('#course').val();
         var body = {id:'',message:m,course:course,teacher:"",title:title};
         $.ajax({
             type: "POST",
@@ -69,22 +72,37 @@ $(document).ready(function(){
         });
     }
 
-    function updateNew(newid){
+    function updateNew(id,title,message,course){
+        $('#add-btn').hide();
+        $('#update-btn').show();
+        $('#message').val(message);
+        $('#title').val(title);
+        $('#course').val(course);
+        $('#id-hidden').val(id);
+        return false;
+    }
+
+    $('#update-btn').click(function(event){
+        event.preventDefault();
+        var id=$('#id-hidden').val();
         var m=$('#message').val();
         var course = $('#title').val();
         var title = $('#course').val();
-        var body = {id:newid,message:m,course:course,teacher:"",title:title};
+        var body = {id:newid,message:m,course:course,teacher:"Pop Ion",title:title};
         $.ajax({
             type:"POST",
             url:"http://localhost:8080/api/news/update/"+newid,
             success:function (data) {
                 getall();
+                $('#update-btn').hide();
+                $('#add-btn').show();
+
             },
             error:function (data,status,xhr) {
                 alert("Error");
             }
         });
-    }
+    })
 
     }
 );
