a031ab2d339edea42d587ee3166c341acb3084ad
Florin Ionita 🔥 <florin.ionita@telenav.com>
2018-01-26 17:18:53 +0200

    Added charts

--- Statistics/Pods/Target Support Files/Pods-Statistics/Pods-Statistics-frameworks.sh
+++ Statistics/Pods/Target Support Files/Pods-Statistics/Pods-Statistics-frameworks.sh
@@ -90,9 +90,13 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PNChart/PNChart.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UICountingLabel/UICountingLabel.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PNChart/PNChart.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/UICountingLabel/UICountingLabel.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
