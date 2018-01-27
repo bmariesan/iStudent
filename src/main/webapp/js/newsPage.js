bb341c837f15a786a4939632fe6889c780d1ad1f
Tudor Stanila <tudorstanila@gmail.com>
2018-01-27 13:38:05 +0200

    Add comments and view all comments works

--- src/main/webapp/js/newsPage.js
+++ src/main/webapp/js/newsPage.js
@@ -1,10 +1,7 @@
-$(document).ready(function () {
+function loadAll(){
     var url = window.location + '';
     var res = url.split("/");
-    // console.log("YOLO");
-    // alert(res);
-    // alert(res.slice(-1).pop());
-
+    $('#list-comm').empty();
     $.ajax({
         type: "GET",
         url: "http://localhost:8080/api/news/comments/" + res.slice(-1).pop(),
@@ -12,13 +9,48 @@ $(document).ready(function () {
         contentType: "application/json; charset=utf-8",
         success: function (data) {
             console.log(data);
-            // getall();
-            // $('#update-btn').hide();
-            // $('#add-btn').show();
+            if(data===undefined){
+                alert("Undefined");
+            }
+            else {
+                console.log(data);
+                if(data.comments.length!=0) {
+                    $.each(data.comments, function (index, value) {
+                        var msg = value.message;
+                        var author = value.posterName;
+                        $('#list-comm').append('<tr><td>' + msg + '</td><td>' + author + '</td></tr>');
+                    });
+                }
+            }
 
         },
         error: function (data, status, xhr) {
             alert("Error");
         }
     });
+}
+$(document).ready(function () {
+
+    loadAll();
+
+    $('#add-comm-btn').click(function(event)
+    {
+        event.preventDefault();
+        var url = window.location + '';
+        var res = url.split("/");
+        var msg = $('#comm-msg').val();
+        var body = {id:'',newsId:res.slice(-1).pop(),message:msg,posterName:'Pop Ion'};
+        $.ajax({
+            type:"POST",
+            url:"http://localhost:8080/api/news/comments",
+            contentType: "application/json; charset=utf-8",
+            data:JSON.stringify(body),
+            success:function(data){
+                loadAll();
+            },
+            error:function(data,status,xhr){
+                alert("Error");
+            }
+        })
+    })
 });
